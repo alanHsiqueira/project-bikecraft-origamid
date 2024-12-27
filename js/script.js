a598cfce3e7a14a3121ff3ada2b315254369e16b
Alan Siqueira <alan.siqueira.25782@aluno.facape.br>
2024-12-27 18:12:54 -0300

    ativar itens orçamento

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+
+// hover header menu ativo
 const links = document.querySelectorAll('.header-menu a'); 
 
 function ativarLink(link) {
@@ -8,5 +10,17 @@ function ativarLink(link) {
   }
 }
 
-
 links.forEach(ativarLink);
+
+// ativar itens orçamento
+
+const parametros = new URLSearchParams(location.search);  
+
+function ativarProduto(parametro){
+  const elemento = document.getElementById(parametro);
+  if(elemento){
+    elemento.checked = true;
+  }
+}
+
+parametros.forEach(ativarProduto);
