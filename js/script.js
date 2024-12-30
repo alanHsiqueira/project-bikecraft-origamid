7e644d948d79993c715c2fb2138b54e7918e8c89
Alan Siqueira <alan.siqueira.25782@aluno.facape.br>
2024-12-30 18:07:24 -0300

    aprendendo como utlizar plugins no projeto

--- js/script.js
+++ js/script.js
@@ -24,3 +24,44 @@ function ativarProduto(parametro){
 }
 
 parametros.forEach(ativarProduto);
+
+//perguntas frequentes
+
+const perguntas = document.querySelectorAll('.perguntas button');
+
+function ativaPergunta(event){
+  const pergunta = event.currentTarget;
+  const controls = pergunta.getAttribute('aria-controls');
+  const resposta = document.getElementById(controls);
+  resposta.classList.toggle('ativa');
+  const ativa = resposta.classList.contains('ativa');
+  pergunta.setAttribute('aria-expanded', ativa);
+}
+function eventoPergunta(pergunta){
+  pergunta.addEventListener('click', ativaPergunta);
+}
+
+perguntas.forEach(eventoPergunta); 
+
+// Galeria de Bicicletas
+
+const galeria = document.querySelectorAll('.bicicleta-imagens img');
+const galeriaContainer = document.querySelector('.bicicleta-imagens');
+
+function trocarImagem(event){
+  const img = event.currentTarget;
+  const media = matchMedia('(max-width:800px)').matches; 
+  if(media){
+    galeriaContainer.prepend(img); 
+  }
+}
+
+function eventosGaleria(img){
+  img.addEventListener('click', trocarimagem);
+}
+galeria.forEach(eventosGaleria);
+
+// Simple Anime
+if(window.SimpleAnime){
+  new SimpleAnime();
+} 
